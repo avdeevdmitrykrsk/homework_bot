720425fb5eb1c9550cf1bd3b84d6595b8ba079ad
avdeevdmitrykrsk <153370353+avdeevdmitrykrsk@users.noreply.github.com>
2024-04-28 16:27:52 +0700

    add

--- homework.py
+++ homework.py
@@ -1,4 +1,7 @@
-...
+import requests
+from datetime import time
+from dotenv import load_dotenv
+
 
 load_dotenv()
 
@@ -18,25 +21,38 @@ HOMEWORK_VERDICTS = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+CHECK_VARIABLES_LIST = [
+    PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID,
+    RETRY_PERIOD, ENDPOINT, HEADERS, HOMEWORK_VERDICTS
+]
+
 
 def check_tokens():
-    ...
-
+    if all(CHECK_VARIABLES_LIST):
+        return True
+    return False
 
 def send_message(bot, message):
     ...
 
 
 def get_api_answer(timestamp):
-    ...
+    from_date = {'from_date': timestamp}
+    check_response(
+        requests.get(
+            ENDPOINT, params=from_date
+        ).json()
+    )
 
 
 def check_response(response):
-    ...
+    if response.status_code == 200:
+        parse_status(response.get('homework')[0])
 
 
 def parse_status(homework):
-    ...
+    homework_name = homework.get('homework_name')
+    verdict = HOMEWORK_VERDICTS.get(homework.get('status'))
 
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
@@ -44,22 +60,21 @@ def parse_status(homework):
 def main():
     """Основная логика работы бота."""
 
-    ...
+    if check_tokens():
 
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    timestamp = int(time.time())
+        bot = telegram.Bot(token=TELEGRAM_TOKEN)
+        timestamp = int(time.time())
+        get_api_answer(timestamp)
 
-    ...
+        while True:
+            try:
 
-    while True:
-        try:
+                ...
 
+            except Exception as error:
+                message = f'Сбой в работе программы: {error}'
+                ...
             ...
-
-        except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            ...
-        ...
 
 
 if __name__ == '__main__':
