c772be9ae416b7841838f16710305467e2c4c2e5
avdeevdmitrykrsk <153370353+avdeevdmitrykrsk@users.noreply.github.com>
2024-05-02 19:57:09 +0700

    pre

--- homework.py
+++ homework.py
@@ -12,8 +12,8 @@ from telebot.apihelper import ApiTelegramException
 
 # Logging settings.
 format = (
-    '%(asctime)s, %(levelname)s, %(message)s, '
-    '%(name)s, func: %(funcName)s, line: %(lineno)d'
+    '%(levelname)s, func: %(funcName)s, line: %(lineno)d, '
+    '%(name)s, %(asctime)s, %(message)s,'
 )
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -29,6 +29,7 @@ PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
+HOMEWORK_NUMBER = 0
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
@@ -39,10 +40,6 @@ HOMEWORK_VERDICTS = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
-HOMEWORK_NUMBER = 0
-EMPTY_VALUE_HOMEWORKS = 0
-VALID_STATUS_CODE = HTTPStatus.OK
-
 
 def check_tokens():
     """Проверка наличия переменных окружения."""
@@ -51,11 +48,12 @@ def check_tokens():
         'PRACTICUM_TOKEN', 'TELEGRAM_TOKEN', 'TELEGRAM_CHAT_ID'
     )
     missing_tokens = [
-        globals().get(var) for var in CHECK_VARIABLES_LIST
-        if not globals().get(var)
+        var for var in CHECK_VARIABLES_LIST if not globals().get(var)
     ]
     if missing_tokens:
-        logger.critical('Не все переменные окружения найдены')
+        logger.critical(
+            f'Переменные окружения не найдены: {missing_tokens}.'
+        )
         sys.exit(
             'Программа остановлена по причине: \nerror: empty variable(s).'
         )
@@ -65,11 +63,17 @@ def check_tokens():
 def send_message(bot, message):
     """Функция отправки сообщения в Telegram."""
     logger.info('Начата отправка сообщения пользователю')
-    bot.send_message(
-        chat_id=TELEGRAM_CHAT_ID,
-        text=message
-    )
-    logger.debug('Сообщение пользователю успешно отправлено')
+    try:
+        bot.send_message(
+            chat_id=TELEGRAM_CHAT_ID,
+            text=message
+        )
+        logger.debug('Сообщение пользователю успешно отправлено')
+    except ApiTelegramException:
+        logger.error(
+            'Ошибка при отправке сообщения пользователю. (main)',
+            exc_info=True
+        )
 
 
 def get_api_answer(timestamp):
@@ -80,13 +84,13 @@ def get_api_answer(timestamp):
         response = requests.get(
             ENDPOINT, headers=HEADERS, params=from_date
         )
-        logger.info('Данные API успешно получены')
     except requests.RequestException as error:
         raise ConnectionError(f'Ошибка соединения: {error}')
-    if response.status_code != VALID_STATUS_CODE:
+    if response.status_code != HTTPStatus.OK:
         raise ValueError(
             f'Ошибка ответа сервера - Status_code: {response.status_code}'
         )
+    logger.info('Данные API успешно получены')
     return response.json()
 
 
@@ -115,26 +119,23 @@ def check_response(response):
             )
         )
     logger.info('Входящие данные проверены. Успех')
-    try:
-        return response.get('homeworks')[HOMEWORK_NUMBER]
-    except IndexError:
-        raise ValueError('Список с домашками пуст')
 
 
 def parse_status(homework):
     """Получаем нужную информацию из ответа."""
     logger.info('Попытка получения статуса домашки')
-    if not homework.get('status'):
-        raise ValueError('Ключ "status" в домашке не обнаружен.')
-    if not homework.get('homework_name'):
-        raise ValueError('Ключ "homework_name" в домашке не обнаружен.')
+    if 'status' not in homework:
+        raise KeyError('Ключ "status" в домашке не обнаружен.')
+    if 'homework_name' not in homework:
+        raise KeyError('Ключ "homework_name" в домашке не обнаружен.')
 
-    homework_status = homework.get('status')
-    homework_name = homework.get('homework_name')
+    homework_status = homework['status']
+    homework_name = homework['homework_name']
 
     if homework_status not in HOMEWORK_VERDICTS:
-        raise KeyError(
-            'Ошибка при получении ключа "status" из словаря вердиктов.'
+        raise ValueError(
+            f'Ошибка при получении ключа "{homework_status}" '
+            'из словаря вердиктов.'
         )
     verdict = HOMEWORK_VERDICTS.get(homework_status)
     message = (
@@ -148,42 +149,29 @@ def parse_status(homework):
 def main():
     """Основная логика работы бота."""
     check_tokens()
-    SENDED_MESSAGE = []
+    sended_message = ''
     bot = TeleBot(token=TELEGRAM_TOKEN)
-    timestamp = int(time.time()) - 100000  # Иначе пустой список с домашками
+    timestamp = int(time.time())
     while True:
         try:
-            message = parse_status(check_response(get_api_answer(timestamp)))
-            if message not in SENDED_MESSAGE:
+            homework = get_api_answer(timestamp)
+            check_response(homework)
+            if not homework['homeworks']:
+                message = 'Список с домашками пуст.'
+            else:
+                message = parse_status(homework['homeworks'][HOMEWORK_NUMBER])
+            logger.info(message)
+            if message not in sended_message:
                 send_message(bot, message)
-                SENDED_MESSAGE.clear()
-                SENDED_MESSAGE.append(message)
-        except ConnectionError:
-            logger.error(
-                'Ошибка соединения: статус код отличный от 200: ',
-                exc_info=True
-            )
-        except ApiTelegramException:
-            logger.error('Ошибка отправки сообщения: ', exc_info=True)
+                sended_message = message
+            timestamp = homework.get('current_date', int(time.time()))
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
             logger.error(message, exc_info=True)
-            if message not in SENDED_MESSAGE:
-                try:
-                    send_message(bot, message)
-                    SENDED_MESSAGE.clear()
-                    SENDED_MESSAGE.append(message)
-                    logger.error(
-                        'Сообщение об ошибке успешно отправлено.',
-                        exc_info=True
-                    )
-                except Exception:
-                    logger.error(
-                        'Ошибка при отправке сообщения пользователю. (main)',
-                        exc_info=True
-                    )
+            if message not in sended_message:
+                send_message(bot, message)
+                sended_message = message
         finally:
-            timestamp = int(time.time()) - 100000
             logger.info(
                 f'Ожидание следующего запроса -- {RETRY_PERIOD} секунд.'
             )
